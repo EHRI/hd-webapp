9d9f3ff76e4557a8bc184ddbb0362f447e94d0a1
KepaJRodriguez <kepa.rodriguez@gmail.com>
2014-01-15 12:27:21 +0100

    Read user query

---
+++ src/main/java/eu/ehri/helpdesk/GetInput.java
@@ -0,0 +1,15 @@
+package eu.ehri.helpdesk;
+
+public class GetInput {
+
+	private String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+}
