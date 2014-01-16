dc820fcd109357adda3dd8ea515359b48b9d03d3
KepaJRodriguez <kepa.rodriguez@gmail.com>
2014-01-16 15:11:33 +0100

    Remove no necessary  properties

--- src/main/java/eu/ehri/helpdesk/GetProperties.java
+++ src/main/java/eu/ehri/helpdesk/GetProperties.java
@@ -3,38 +3,10 @@ package eu.ehri.helpdesk;
 import java.io.IOException;
 import java.util.Properties;
 
-/*
- * Later separate in collection descriptions folder
- * knowledge, features, etc.
- */
+ // Only relevant property is the TreeTagger Folder
 
 public class GetProperties {
 
-	
-	public String getModelFolder(){
-		Properties prop = new Properties();	
-		
-		try {
-			prop.load(getClass().getResourceAsStream("config.properties"));
-		}catch (IOException ex) {
-			ex.printStackTrace();
-	    }
-		final String MODEL_FOLDER = prop.getProperty("model_folder");
-		
-		return MODEL_FOLDER;
-	}
-	public String getDescriptionsFolder(){
-		Properties prop = new Properties();	
-		
-		try {
-			prop.load(getClass().getResourceAsStream("config.properties"));
-		}catch (IOException ex) {
-			ex.printStackTrace();
-	    }
-		final String DESCRIPTIONS_FOLDER = prop.getProperty("descriptions_folder");
-		
-		return DESCRIPTIONS_FOLDER;
-	}
 	public String getTreeTaggerFolder(){
 		Properties prop = new Properties();	
 		
