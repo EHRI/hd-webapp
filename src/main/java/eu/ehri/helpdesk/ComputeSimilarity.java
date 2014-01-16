f150e2484c78bb2a0d6b0ef9245443497cd0d50f
KepaJRodriguez <kepa.rodriguez@gmail.com>
2014-01-16 15:13:18 +0100

    Read resources from resources folder and use minimal similarity

--- src/main/java/eu/ehri/helpdesk/ComputeSimilarity.java
+++ src/main/java/eu/ehri/helpdesk/ComputeSimilarity.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,38 +16,36 @@ import java.util.regex.Pattern;
 
 public class ComputeSimilarity {
 
+	// Read vector of each institution
+	// Add new institutions here
+	InputStream niod = getClass().getResourceAsStream("/Model/wiener.tfidf");
+	InputStream wiener = getClass().getResourceAsStream("/Model/niod.tfidf");
+	InputStream yadvashem = getClass().getResourceAsStream(
+			"/Model/yadvashem.tfidf");
+	InputStream jewishmuseumprag = getClass().getResourceAsStream(
+			"/Model/jewishmuseumprag.tfidf");
 
-	static GetProperties property = new GetProperties();
-	final static String modelFolder = property.getModelFolder();
-	final static File files = new File(modelFolder);
+	// List of vectors representing archives
+	List<InputStream> vectors = new ArrayList<InputStream>();
 
-	//provisional change to list for display
-//	public static HashMap<String, Double> computeSimilarity(
-	public static List<String> computeSimilarity(		
-			HashMap<String, Double> userquery) {
-		
-		Pattern tfidfFile = Pattern.compile(".*tfidf");
-		List<String> fileList = new ArrayList<String>();
-		//HashMap<String, HashMap<String, Double>> institutions = new HashMap<String, HashMap<String, Double>>();
+	// provisional change to list for display
+	public List<String> computeSimilarity(HashMap<String, Double> userquery) {
+
+		vectors.add(niod);
+		vectors.add(yadvashem);
+		vectors.add(wiener);
+		vectors.add(jewishmuseumprag);
+
 		HashMap<String, Double> institutionsRelevance = new HashMap<String, Double>();
-		
-		for (final File fileEntry : files.listFiles()) {
-			Matcher matcherText = tfidfFile.matcher(fileEntry.toString());
-			if (matcherText.find()) {
-				fileList.add(fileEntry.getName());
-				System.out.println(fileEntry.getName());
-			}
-		}
 
-		for (int i = 0; i < fileList.size(); i++) {
+		for (int i = 0; i < vectors.size(); i++) {
 			List<String> fileLines = new ArrayList<String>();
 			HashMap<String, Double> institutionFeatures = new HashMap<String, Double>();
 			String currentLine;
 			BufferedReader br = null;
 
 			try {
-				br = new BufferedReader(new FileReader(modelFolder
-						+ fileList.get(i)));
+				br = new BufferedReader(new InputStreamReader(vectors.get(i)));
 				while ((currentLine = br.readLine()) != null) {
 					fileLines.add(currentLine);
 				}
@@ -63,33 +63,37 @@ public class ComputeSimilarity {
 
 			Double similarity = CosineSimilarity.computeCosine(userquery,
 					institutionFeatures);
-			System.out.println(fileList.get(i) + " --> " + similarity);
-			
-			if (fileList.get(i).equals("yadvashem.tfidf")){
-				institutionsRelevance.put("Yad Vashem", similarity);
-			}
-			if (fileList.get(i).equals("niod.tfidf")){
-				institutionsRelevance.put("NIOD", similarity);
-			}
-			if (fileList.get(i).equals("wiener.tfidf")){
-				institutionsRelevance.put("Wiener Library", similarity);
-			}
-			if (fileList.get(i).equals("jewishmuseumprag.tfidf")){
-				institutionsRelevance.put("Jewish Museum Prag", similarity);
+			System.out
+					.println(vectors.get(i).toString() + " --> " + similarity);
+
+			if (similarity > 0.008) {
+				if (vectors.get(i).equals(yadvashem)) {
+					institutionsRelevance.put("Yad Vashem", similarity);
+				}
+				if (vectors.get(i).equals(niod)) {
+					institutionsRelevance.put("NIOD", similarity);
+				}
+				if (vectors.get(i).equals(wiener)) {
+					institutionsRelevance.put("Wiener Library", similarity);
+				}
+				if (vectors.get(i).equals(jewishmuseumprag)) {
+					institutionsRelevance.put("Jewish Museum Prag", similarity);
+				}
 			}
 		}
 
-
-		HashMap<String,Double> newHashMap = AuxiliaryMethods.sortByValue(institutionsRelevance);
+		HashMap<String, Double> newHashMap = AuxiliaryMethods
+				.sortByValue(institutionsRelevance);
 		AuxiliaryMethods.sortByValue(institutionsRelevance);
-		
+
 		List<String> relevance = new ArrayList<String>();
-		for (String key : newHashMap.keySet()){
-			String item = key + " --> " + institutionsRelevance.get(key).toString();
+		for (String key : newHashMap.keySet()) {
+			String item = key + " --> "
+					+ institutionsRelevance.get(key).toString();
 			relevance.add(item);
 		}
-		
-		return relevance;		
+
+		return relevance;
 
 	}
 }
