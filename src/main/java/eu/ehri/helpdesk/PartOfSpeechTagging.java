cb387dd0118bf7806bc9e096bb0000a4e51ba890
KepaJRodriguez <kepa.rodriguez@gmail.com>
2014-01-21 10:56:13 +0100

    No static call of GetProperties

--- src/main/java/eu/ehri/helpdesk/PartOfSpeechTagging.java
+++ src/main/java/eu/ehri/helpdesk/PartOfSpeechTagging.java
@@ -10,37 +10,29 @@ import org.annolab.tt4j.TreeTaggerWrapper;
 
 public class PartOfSpeechTagging {
 
-	static GetProperties property = new GetProperties();
+	GetProperties property = new GetProperties();
 	final String treeTaggerFolder = property.getTreeTaggerFolder();
-	
 	public List<String> annotatePOSpeech(List<String> tokenized)
 			throws IOException, TreeTaggerException {
-		// Point TT4J to the TreeTagger installation directory. The executable
-		// is expected
-		// in the "bin" subdirectory - in this example at
-		// "/opt/treetagger/bin/tree-tagger"
 		
-		
-		
-		//System.setProperty("treetagger.home", "/home/kepa/Software/TreeTager/");
+		System.out.println("Tree Tager Folder in " + treeTaggerFolder);
+			
 		System.setProperty("treetagger.home", treeTaggerFolder);
 		TreeTaggerWrapper<String> tt = new TreeTaggerWrapper<String>();
 		final List<String> outputTreeTagger = new ArrayList<String>();
 
 		try {
-//			tt.setModel(treeTaggerFolder + "lib/english.par:utf8");
 			tt.setModel(treeTaggerFolder + "lib/english.par:utf8");
 			tt.setHandler(new TokenHandler<String>() {
 				public void token(String token, String pos, String lemma) {
 					outputTreeTagger.add(token + "\t" + pos + "\t" + lemma);
-					// System.out.println(token + "\t" + pos + "\t" + lemma);
 				}
 			});
 			tt.process(tokenized);
 		} finally {
 			tt.destroy();
 		}
-		// System.out.println("SIZE: " + outputTreeTagger.size());
+		
 		return outputTreeTagger;
 	}
 
