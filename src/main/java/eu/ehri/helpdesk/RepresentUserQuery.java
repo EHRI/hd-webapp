73641915e8add84cac3d9ff716221faf88790e6b
KepaJRodriguez <kepa.rodriguez@gmail.com>
2014-03-07 21:29:07 +0200

    Deleted unused imports

--- src/main/java/eu/ehri/helpdesk/RepresentUserQuery.java
+++ src/main/java/eu/ehri/helpdesk/RepresentUserQuery.java
@@ -3,7 +3,6 @@ package eu.ehri.helpdesk;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 public class RepresentUserQuery {
